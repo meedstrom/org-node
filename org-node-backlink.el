554b378f2cd8b392709c32650648fa27e2d3bac5
Martin Edström <meedstrom91@gmail.com>
2024-05-05 15:32:18 +0200

    Add fast ws-butler based :BACKLINKS: updater

--- org-node-backlink.el
+++ org-node-backlink.el
@@ -9,13 +9,14 @@
   :group 'org-node
   (if org-node-backlink-mode
       (progn
-        (add-hook 'before-save-hook #'org-node-backlink--update-buffer-safe 0 t)
         (add-hook 'org-roam-post-node-insert-hook #'org-node-backlink--add-in-target-a -99 t)
         (add-hook 'org-node-insert-link-hook #'org-node-backlink--add-in-target-a -99 t)
-        ;; It seems advices cannot be buffer-local, but it's OK, this function
-        ;; will do nothing if this mode isn't active in current buffer.
-        (advice-add 'org-insert-link :after #'org-node-backlink--add-in-target-a))
-    (remove-hook 'before-save-hook #'org-node-backlink--update-buffer-safe t)
+        ;; It seems advices cannot be buffer-local, but it's OK, these fns
+        ;; do nothing if this mode isn't active in the current buffer.
+        (advice-add 'org-insert-link :after
+                    #'org-node-backlink--add-in-target-a)
+        (advice-add 'ws-butler-before-save :before
+                    #'org-node-backlink--update-changed-parts-of-buffer))
     (remove-hook 'org-roam-post-node-insert-hook #'org-node-backlink--add-in-target-a t)
     (remove-hook 'org-node-insert-link-hook #'org-node-backlink--add-in-target-a t)))
 
@@ -35,7 +36,7 @@ as the user command \\[org-node-backlink-regret]."
             (and
              (y-or-n-p (format "Edit the %d files found in `org-id-locations'?"
                                (length org-node-backlink--fix-cells)))
-             (y-or-n-p "You understand that your auto git-commit systems and similar will probably run?")))
+             (y-or-n-p "You understand that this may trigger your auto git-commit systems and similar?")))
     (let ((find-file-hook nil)
           (org-mode-hook nil)
           (after-save-hook nil)
@@ -47,16 +48,17 @@ as the user command \\[org-node-backlink-regret]."
       (dotimes (_ 1000)
         (when-let ((cell (pop org-node-backlink--fix-cells)))
           (message
-           "Adding/updating :BACKLINKS:... (you can stop and resume anytime) (%d) %s"
+           "Adding/updating :BACKLINKS:... (you may quit and resume anytime) (%d) %s"
            (cl-incf org-node-backlink--fix-ctr) (car cell))
-          (org-with-file-buffer (car cell)
-            (org-with-wide-buffer
-             (org-node-backlink--update-buffer (cdr cell) remove))
-            (and org-file-buffer-created
-                 (buffer-modified-p)
-                 (let ((save-silently t)
-                       (inhibit-message t))
-                   (save-buffer)))))))
+          (delay-mode-hooks
+            (org-with-file-buffer (car cell)
+              (org-with-wide-buffer
+               (org-node-backlink--update-buffer remove))
+              (and org-file-buffer-created
+                   (buffer-modified-p)
+                   (let ((save-silently t)
+                         (inhibit-message t))
+                     (save-buffer))))))))
     (if org-node-backlink--fix-cells
         ;; Keep going
         (run-with-timer 1 nil #'org-node-backlink-fix-all remove)
@@ -72,66 +74,99 @@ files known to `org-id-locations'."
 
 ;;; Single file update
 
-;; TODO Let it update giant files quickly.  Now it just gives up and gets out
-;; of the way if there are >100 links.  But I'd like to support truly giant
-;; files.  Take a page from the ws-butler concept: only update the parts of the
-;; file that changed.
-(defun org-node-backlink--update-buffer-safe ()
-  "Wrapper for `before-save-hook', and avoids signaling an error."
-  (interactive)
-  (let ((n-links
-         (save-mark-and-excursion
-           (goto-char (point-min))
-           (cl-loop while (re-search-forward org-link-bracket-re nil t)
-                    count t))))
-    ;; If there's a ton of links in this file, don't do anything
-    (unless (> n-links 100)
-      ;; Catch and drop any error
-      (condition-case err
-          (org-node-backlink--update-buffer)
-        ((t error)
-         (message "org-node-backlink failed with message %s %s"
-                  (car err) (cdr err)))))))
+(defun org-node-backlink--update-subtree-here (&optional remove)
+  "Assumes point is at an :ID: line!
+Update the :BACKLINKS: property.  With arg REMOVE, remove it instead."
+  (org-entry-delete nil "CACHED_BACKLINKS")  ;; Old name, get rid of it
+  (if remove
+      (org-entry-delete nil "BACKLINKS")
+    (skip-chars-forward "[:space:]")
+    (let* ((id (buffer-substring-no-properties
+                (point) (+ (point) (skip-chars-forward "^ \n"))))
+           (ROAM_REFS
+            (org-node-get-refs
+             (or (gethash id org-nodes)
+                 (if (string-blank-p id)
+                     (user-error "Blank ID property in %s" buffer-file-name)
+                   (error "ID exists but not scanned by org-node for some reason, bad syntax? %s in file %s"
+                          id buffer-file-name)))))
+           (reflinks (--map (gethash it org-node--reflinks-table)
+                            ROAM_REFS))
+           (backlinks (gethash id org-node--links-table))
+           (combined
+            (->> (append reflinks backlinks)
+                 (--map (plist-get it :src))
+                 ;; TODO Not sure how there are nils, look into it
+                 (remq 'nil)
+                 (-uniq)
+                 (-sort #'string-lessp)
+                 ;; At this point we have a sorted list of ids (sorted
+                 ;; only to reduce git diffs when the order changes) of
+                 ;; every node that links to here
+                 (--map (org-link-make-string
+                         (concat "id:" it)
+                         (org-node-get-title
+                          (or (gethash it org-nodes)
+                              (error "ID in backlink tables not known to main org-nodes table: %s"
+                                     it)))))))
+           (link-string (string-join combined "  ")))
+      (if combined
+          (unless (equal link-string (org-entry-get nil "BACKLINKS"))
+            (org-entry-put nil "BACKLINKS" link-string))
+        (org-entry-delete nil "BACKLINKS")))))
 
-(defun org-node-backlink--update-buffer (&optional ids remove)
-  (let ((ids (or ids (cdr (assoc buffer-file-name
-                                 (org-id-hash-to-alist org-id-locations))))))
-    (save-excursion
-      (goto-char (point-min))
-      (dolist (id ids)
-        ;; Allow it to fail, as it may just mean the buffer is narrowed
-        (when-let ((pos (org-find-property "ID" id)))
-          (goto-char pos)
-          ;; Old name, get rid of it
-          (org-entry-delete nil "CACHED_BACKLINKS")
-          (if remove
-              (org-entry-delete nil "BACKLINKS")
-            (let* ((ROAM_REFS (ignore-errors
-                                (org-node-get-refs (gethash id org-nodes))))
-                   (reflinks (--map (gethash it org-node--reflinks-table)
-                                    ROAM_REFS))
-                   (backlinks (gethash id org-node--links-table))
-                   (combined
-                    (->> (append reflinks backlinks)
-                         (--map (plist-get it :src))
-                         ;; TODO Not sure how there are nils, look into it
-                         (remq 'nil)
-                         (-uniq)
-                         (-sort #'string-lessp)
-                         ;; At this point we have a sorted list of ids (sorted
-                         ;; only for idempotency)
-                         (--map (org-link-make-string
-                                 (concat "id:" it)
-                                 (org-node-get-title
-                                  (gethash it org-nodes))))))
-                   (link-string (string-join combined "  ")))
-              (if combined
-                  (unless (equal link-string (org-entry-get nil "BACKLINKS"))
-                    (org-entry-put nil "BACKLINKS" link-string))
-                (org-entry-delete nil "BACKLINKS")))))))))
+(defun org-node-backlink--update-buffer (&optional remove markers)
+  (save-excursion
+    (goto-char (point-min))
+    (if markers
+        (dolist (marker markers)
+          (goto-char (marker-position marker))
+          (set-marker marker nil)
+          (org-node-backlink--update-subtree-here remove))
+      (let ((case-fold-search t))
+        (while (re-search-forward "^[[:space:]]*:id: " nil t)
+          (org-node-backlink--update-subtree-here remove))))))
+
+(defvar-local org-node-backlink--subtree-markers (list))
+
+(defun org-node-backlink--flag-subtrees-with-changes (_prop start end)
+  (save-excursion
+    (goto-char start)
+    (let ((case-fold-search t))
+      ;; We're in a region where ws-butler detected a change, but the region
+      ;; will almost never envelop the preceding heading, so search back for it
+      (save-excursion
+        (when (re-search-backward "^[[:space:]]*:id: " nil t)
+          (goto-char (match-end 0))
+          (push (point-marker) org-node-backlink--subtree-markers)))
+      ;; ...and if the region of detected change is massive, spanning multiple
+      ;; subtrees, find each one
+      (while (re-search-forward "^[[:space:]]*:id: " end t)
+        (push (point-marker) org-node-backlink--subtree-markers)))))
+
+(defun org-node-backlink--update-changed-parts-of-buffer ()
+  (when org-node-backlink-mode
+    (unless ws-butler-mode
+      (message "If saving a big file is slow, see org-node README for a fix based on ws-butler"))
+    (while-let ((marker (pop org-node-backlink--subtree-markers)))
+      (set-marker marker nil))
+    ;; Catch any error because this runs at `before-save-hook' which MUST fail
+    ;; gracefully and let the user save anyway.
+    (condition-case err
+        (progn
+          (ws-butler-map-changes #'org-node-backlink--flag-subtrees-with-changes)
+          (org-node-backlink--update-buffer
+           nil org-node-backlink--subtree-markers))
+      (( error user-error debug )
+       (lwarn 'org-node :error
+              "org-node-backlink--update-changed-parts-of-buffer: %S" err)))))
+
+
+
 
 
 ;;; Link-insertion advice
+;; TODO: Simplify
 
 (defvar org-node-backlink--fails nil
   "List of IDs that could not be resolved.")
