6a26290181acf7a3d13f2db61a07a11407175055
Martin Edström <meedstrom91@gmail.com>
2024-06-01 15:43:29 +0200

    Revert "fix: make first run of org-node-find wait for completed scan"
    This reverts commit 3a2b22828d03c2f3606dd938438edb5821706f89.

--- org-node.el
+++ org-node.el
@@ -28,11 +28,13 @@
 
 ;;; Code:
 
+;; TODO Do not clear the tables until all processes have returned without fail
 ;; TODO What happens when we move a subtree to a different file but save the destination before saving the origin file?
 ;; TODO Annotations for completion?
 ;; TODO Completion categories? https://github.com/alphapapa/org-ql/issues/299
 ;; TODO Command to grep across all files
 ;; TODO Command to explore feedback arc sets
+;; TODO A test suite
 
 (require 'org-node-common)
 (require 'org-node-cache)
@@ -283,7 +285,7 @@ gets some necessary variables."
 To behave like `org-roam-node-find' when creating new nodes, set
 `org-node-creation-fn' to `org-node-new-by-roam-capture'."
   (interactive)
-  (org-node-cache-ensure t)
+  (org-node-cache-ensure)
   (let* ((input (completing-read "Node: " org-node-collection
                                  () () () 'org-node-hist))
          (node (gethash input org-node-collection)))
@@ -380,7 +382,7 @@ it to `org-node-insert-link-hook'."
 ;;;###autoload
 (defun org-node-random ()
   (interactive)
-  (org-node-cache-ensure t)
+  (org-node-cache-ensure)
   (org-node--goto (nth (random (hash-table-count org-nodes))
                        (hash-table-values org-nodes))))
 
