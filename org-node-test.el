2bfea9f9486b34b344f9be3152275c3b6d739360
Martin Edström <meedstrom91@gmail.com>
2024-07-24 14:50:39 +0200

    style: GPL in each source file

--- org-node-test.el
+++ org-node-test.el
@@ -1,11 +1,27 @@
 ;;; org-node-test.el -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2024 Martin Edström
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 (require 'ert)
 (require 'dash)
 (require 'org-node)
-(require 'org-node-fakeroam)
 (require 'org-node-worker)
 (require 'org-node-backlink)
+;; (require 'org-node-fakeroam)
+
 
 ;; (-all-p #'org-node-link-p (apply #'append (hash-table-values org-node--dest<>links)))
 
