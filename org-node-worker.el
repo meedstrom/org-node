4bae11e7e864765eba547b816dadc9eb277fbc11
Martin Edström <meedstrom91@gmail.com>
2024-07-24 14:50:39 +0200

    style: GPL in each source file

--- org-node-worker.el
+++ org-node-worker.el
@@ -1,5 +1,20 @@
 ;;; org-node-worker.el --- Gotta go fast -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2024 Martin Edström
+;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
 (eval-when-compile
   (require 'cl-macs)
   (require 'subr-x))
