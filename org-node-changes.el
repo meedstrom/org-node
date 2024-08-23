75b022d4f40e9bc91c716f204a1d7b610877daa1
Martin Edström <meedstrom91@gmail.com>
2024-08-23 16:57:53 +0200

    fix

--- org-node-changes.el
+++ org-node-changes.el
@@ -147,11 +147,11 @@ hardcoded strings."
 (define-obsolete-function-alias
   'org-node--create #'org-node-create "2024-08-21")
 
-(define-obsolete-variable-alias
-  'org-node-creation-fn 'org-node-new-node-fn "2024-08-22")
+;; (define-obsolete-variable-alias
+;;   'org-node-creation-fn 'org-node-new-node-fn "2024-08-22")
 
-(define-obsolete-variable-alias
-  'org-node-creation-hook 'org-node-new-node-hook "2024-08-22")
+;; (define-obsolete-variable-alias
+;;   'org-node-creation-hook 'org-node-new-node-hook "2024-08-22")
 
 ;; (define-obsolete-function-alias
 ;;   'org-node-create #'org-node-new-node "2024-08-22")
