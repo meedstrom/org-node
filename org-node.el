c1e8d8b1e8bf9463c7b97f290a8307184a19c041
Martin Edström <meedstrom91@gmail.com>
2024-12-01 21:12:55 +0100

    chore: Bump dependency el-job

--- org-node.el
+++ org-node.el
@@ -18,7 +18,7 @@
 ;; Author:           Martin Edström <meedstrom91@gmail.com>
 ;; Created:          2024-04-13
 ;; Keywords:         org, hypermedia
-;; Package-Requires: ((emacs "28.1") (compat "30") (el-job "0.3.11") (llama "0.4.0"))
+;; Package-Requires: ((emacs "28.1") (compat "30") (el-job "0.3.14") (llama "0.4.0"))
 ;; URL:              https://github.com/meedstrom/org-node
 
 ;; NOTE: Looking for Package-Version?
